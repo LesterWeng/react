5a089fcbc6e142fcf98fc60c42625fbaa3007662
wenglixin <lixin.weng@amh-group.com>
2022-07-13 14:31:24 +0800

    docs:

--- packages/react-reconciler/src/ReactFiberConcurrentUpdates.old.js
+++ packages/react-reconciler/src/ReactFiberConcurrentUpdates.old.js
@@ -50,6 +50,7 @@ let concurrentQueuesIndex = 0;
 
 let concurrentlyUpdatedLanes: Lanes = NoLanes;
 
+// API-triggerPhase:包括了处理this.setState生成的shared.pending
 export function finishQueueingConcurrentUpdates(): void {
   const endIndex = concurrentQueuesIndex;
   concurrentQueuesIndex = 0;
